a66c150ed3c0cff3868e956fee095448239f7073
Dhruv Khokhar <145288137+Dhruvk6268@users.noreply.github.com>
2026-04-20 18:03:46 +0530

    Delete php directory

--- php/db_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    require_once __DIR__ . '/../config/db.php';
-    echo "Database connection successful!";
-    $pdo = null;
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
